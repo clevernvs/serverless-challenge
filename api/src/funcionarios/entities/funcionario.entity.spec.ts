e634668f09f5146c4391553350046eb99d91b0b0
Cleverton  Neves <clever.nvs@gmail.com>
2024-02-22 06:26:22 -0300

    feat: Incluir Docker e containeres

--- api/src/funcionarios/entities/funcionario.entity.spec.ts
+++ api/src/funcionarios/entities/funcionario.entity.spec.ts
@@ -12,4 +12,16 @@ describe('Funcionario Testes', () => {
         expect(funcionario.idade).toBe(35);
         expect(funcionario.cargo).toBe('Desenvolvedor');
     })
+
+    it('Criar um funcionario', () => {
+        const funcionario = new Funcionario({
+            nome: 'Fulano',
+            idade: 35,
+            cargo: 'Desenvolvedor',
+        });
+
+        expect(funcionario.nome).toBe('Fulano');
+        expect(funcionario.idade).toBe(35);
+        expect(funcionario.cargo).toBe('Desenvolvedor');
+    })
 })
