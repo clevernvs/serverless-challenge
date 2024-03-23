6b18160a4e161a555cc33d7bcc1e1b399242799e
Cleverton  Neves <clever.nvs@gmail.com>
2024-03-23 11:31:11 -0300

    feat: Refatoração dos containeres

---
+++ api/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+npm ls | grep ERR
+
+# Define diretórios de origem e destino
+origem="/home/node/cache/node_modules/"
+destino="/home/node/app/api/node_modules/"
+
+# Verifica se os diretórios de origem e destino existem
+if [ ! -d "$origem" ]; then
+  echo "Diretório de origem não encontrado: $origem"
+  exit 1
+fi
+
+if [ ! -d "$destino" ]; then
+  echo "Diretório de destino não encontrado: $destino"
+  exit 1
+fi
+
+# Sincroniza os módulos node
+rsync -arv "$origem" "$destino"
+if [ $? -ne 0 ]; then
+  echo "Erro ao sincronizar os módulos node."
+  exit 1
+fi
+
+# Inicia o servidor Angular
+npm run start:dev
+if [ $? -ne 0 ]; then
+  echo "Erro ao iniciar o servidor Nest JS."
+  exit 1
+fi
+
+# Script concluído com sucesso
+exit 0
